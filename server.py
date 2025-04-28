5f7080f8e4454e0ecf3fd3d24dcce9757ffd532e
godji195 <pattanaphichai2@gmail.com>
2025-04-28 08:11:49 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import random
 import re
 
 app = Flask(__name__)
+CORS(app)  # เปิดใช้งาน CORS
 
 # ข้อความทำนายสำหรับแต่ละเดือน
 monthly_fortunes = [
